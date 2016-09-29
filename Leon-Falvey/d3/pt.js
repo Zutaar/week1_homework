feede57a2e5a18b833073d099690da91b4607c90
Zutaar <leon.falvey@gmail.com>
2016-09-29 21:18:23 +1000

    pt.js draft of main task but not bonus complete

--- Leon-Falvey/d3/pt.js
+++ Leon-Falvey/d3/pt.js
@@ -3,50 +3,84 @@ var train = {
                   'Burnley','Hawthorn','Glenferrie'],
   glenWaverly : ['Flagstaff','Melbourne Central','Parliament',
                   'Richmond','Kooyong','Tooronga'],
-  sandringham : ['Southen Cross','Richmond','South Yarra',
+  sandringham : ['Southern Cross','Richmond','South Yarra',
                   'Prahran', 'Windsor'],
-  'Richmond' : [1,3,1]
 };
 var intersections = {
   'Richmond':['alamein', 'glenWaverly', 'sandringham']
 };
 var lines = ['alamein', 'glenWaverly', 'sandringham'];
 
-var origin="Flinders Street";//prompt("Enter your origin");
-var destination="Burnley";//prompt("Enter your destination");
+var origin= "Glenferrie";//prompt("Which station are you starting from?");//prompt("Enter your origin");
+
+
+
+var destination= "Prahran";//prompt("Where is your destination?");//prompt("Enter your destination");
 
 var currentLine=-1; // the current line index
 var pos=-1;
 var posLine=-1;
 var dest=-1;
 var destLine=-1;
-
+var nextStop=-1;
 
 for (var i=0;i<lines.length;i++) {
-  if (currentLine<train[lines[i]].indexOf(origin)) {
-    currentLine=i;
-    //console.log("on " + lines[i]);
-    pos=train[lines[i]].indexOf(origin);
-    //console.log("at " + train[lines[i]][pos]);
+  if (currentLine<train[lines[i]].indexOf(destination)) {
     dest=train[lines[i]].indexOf(destination);
-    //console.log("to " + train[lines[i]][dest]);
+    destLine=i;
+  }
+}
+if (origin!=="Richmond") {
+  for (var i=0;i<lines.length;i++) {
+    if (currentLine<train[lines[i]].indexOf(origin)) {
+      currentLine=i;
+      //console.log("on " + lines[i]);
+      pos=train[lines[i]].indexOf(origin);
+      //console.log("at " + train[lines[i]][pos]);
+    }
+  }
+} else {
+  currentLine=destLine;
+  pos=train[lines[currentLine]].indexOf(origin);
+}
+
+if (destLine===currentLine) {
+  nextStop=train[lines[currentLine]].indexOf(destination);
+} else {
+  for (var i=0;i<train[lines[currentLine]].length;i++) {
+    if (train[lines[currentLine]][i]==="Richmond") {
+      nextStop=i;
+    }
   }
 }
 
 
+var currentStation=origin; // name of current station. Initially set to origin.
+var route = [origin]; // creates the route taken and starts off with origin.
+var numStops = 0; //records number of stops taken
 
-
-
-if (pos>dest) {
-  pos=train[lines[currentLine]].length-1-pos;
-  dest=train[lines[currentLine]].length-1-dest;
-  train[lines[currentLine]].reverse();
+while (currentStation!==destination) {
+   if (currentStation!==nextStop) {
+    if (pos>nextStop) { // reverses array
+      pos=train[lines[currentLine]].length-1-pos;
+      nextStop=train[lines[currentLine]].length-1-nextStop;
+      train[lines[currentLine]].reverse();
+    }
+    for (var i=1;i+pos<=nextStop;i++) {
+      currentStation=train[lines[currentLine]][pos+i];
+      // console.log(currentStation);
+      route.push(currentStation);
+      numStops++;
+      if (currentStation==="Richmond" && currentLine!==destLine) {break;}
+    }
+  }
+  //Changes line if necessary
+  if (currentLine!==destLine) {
+    pos = train[lines[destLine]].indexOf(currentStation);
+    currentLine=destLine;
+    nextStop=dest;
+  }
 }
-var route = [origin];
-var numStops = 0;
-for (var i=1;i+pos<=dest;i++) {
-  route.push(train[lines[currentLine]][pos+i]);
-  numStops++;
-}
+
 console.log(route.join(" ---> "));
 console.log(numStops + " stops total.")
